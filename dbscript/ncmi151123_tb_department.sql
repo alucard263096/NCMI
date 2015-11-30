9bbe7fcc362b87643fbe48c120b74d3ecab97ec4
alucard263096 <alucard263096@126.com>
2015-12-01 00:33:05 +0800

    depart ment table update

--- dbscript/ncmi151123_tb_department.sql
+++ dbscript/ncmi151123_tb_department.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `ncmi151123` /*!40100 DEFAULT CHARACTER SET utf8 
 USE `ncmi151123`;
 -- MySQL dump 10.13  Distrib 5.6.19, for Win32 (x86)
 --
--- Host: localhost    Database: ncmi151123
+-- Host: 120.24.239.49    Database: ncmi151123
 -- ------------------------------------------------------
 -- Server version	5.6.21-enterprise-commercial-advanced-log
 
@@ -37,6 +37,7 @@ CREATE TABLE `tb_department` (
   `remarks` varchar(500) DEFAULT NULL,
   `status` varchar(10) DEFAULT NULL,
   `college_id` int(11) DEFAULT NULL,
+  `subcategory_str` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -47,7 +48,7 @@ CREATE TABLE `tb_department` (
 
 LOCK TABLES `tb_department` WRITE;
 /*!40000 ALTER TABLE `tb_department` DISABLE KEYS */;
-INSERT INTO `tb_department` VALUES (1,'2015-11-25 22:58:57',1,'2015-11-25 22:58:57',1,'普通外科','','','3,4,5','','A',1),(2,'2015-11-25 22:59:14',1,'2015-11-25 22:59:14',1,'泌尿外科','','','5','','A',1),(3,'2015-11-25 22:59:52',1,'2015-11-25 22:59:52',1,'癌症内科','','','1','','A',2),(4,'2015-11-25 23:00:02',1,'2015-11-25 23:00:02',1,'肿瘤内科','','','2','','A',2),(5,'2015-11-25 23:00:31',1,'2015-11-25 23:00:31',1,'全科','','','1,2,3,4,5','','A',3);
+INSERT INTO `tb_department` VALUES (1,'2015-11-25 22:58:57',1,'2015-11-30 22:03:29',1,'普通外科','','','3,4,5','','A',1,',心脏外科疾病-心脏外科,普通外科-普通外科,普通外科-超声刀外科'),(2,'2015-11-25 22:59:14',1,'2015-11-25 22:59:14',1,'泌尿外科','','','5','','A',1,',普通外科-超声刀外科'),(3,'2015-11-25 22:59:52',1,'2015-11-25 22:59:52',1,'癌症内科','','','1','','A',2,',癌症肿瘤-癌症'),(4,'2015-11-25 23:00:02',1,'2015-11-25 23:00:02',1,'肿瘤内科','','','2','','A',2,',癌症肿瘤-肿瘤'),(5,'2015-11-25 23:00:31',1,'2015-11-25 23:00:31',1,'全科','','','1,2,3,4,5','','A',3,',癌症肿瘤-癌症,癌症肿瘤-肿瘤,心脏外科疾病-心脏外科,普通外科-普通外科,普通外科-超声刀外科');
 /*!40000 ALTER TABLE `tb_department` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -60,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-26 23:58:57
+-- Dump completed on 2015-12-01  0:32:39
