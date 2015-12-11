c69045102aeac48e76d6ce67713b76114087835a
Steve Cai <steve.cai@morningstar.com>
2015-12-11 14:59:59 +0800

    会员模块的医生关注列表显示已经完成

---
+++ Website/Member/followdoctorlist.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Created on 2012-6-30
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+  require '../include/common.inc.php';
+  include ROOT.'/include/init.inc.php';
+  include ROOT.'/include/member.inc.php';
+  require ROOT.'/classes/datamgr/doctor.cls.php';
+
+  $search=$_REQUEST["search"];
+  $smarty->assign("searchkeyword",$search);
+  
+  $currentpage=getPageNumber();
+  $doctorlist=$doctorMgr->getFollowDoctorList($member["id"],$search,$currentpage);
+  $smarty->assign("list",$doctorlist);
+
+  
+  $doctorsum=$doctorMgr->getFollowDoctorListPageCount($member["id"],$search,$currentpage);
+  $smarty->assign("doctor_count",$doctorsum);
+  $smarty->assign("current_page",$currentpage);
+  $smarty->assign("lastpage",$currentpage);
+  $smarty->assign("page_arr",getPageNumberCodeArray($doctorsum,$currentpage,18));
+
+  
+	$smarty->assign("menuid","follow");
+  
+  $smarty->display(ROOT.'/templates/Member/followdoctorlist.html');
+  
+?>
--- Website/classes/datamgr/doctor.cls.php
+++ Website/classes/datamgr/doctor.cls.php
@@ -51,13 +51,20 @@ inner join tb_hospital h on d.hospital_id=h.id
 		return $result;
 	}
 
-	public function getDoctorList($search,$page){
+	public function getDoctorListSearchSql(){
+		$sql="select distinct d.id,d.name,d.photo,d.position
+from tb_doctor d
+inner join tb_hospital h on d.hospital_id=h.id and h.status='A'
+inner join tb_department dp on d.department_id=dp.id and dp.status='A'
+inner join tb_college c on dp.college_id=c.id and c.status='A'
+inner join rc_department_subcategory rc_ds on dp.id=rc_ds.pid
+inner join tb_subcategory s on rc_ds.fid=s.id
+inner join tb_category cat on cat.id=s.category_id ";
 		
-		$startrow=($page-1)*18;
-		if($startrow>0){
-			//$startrow=$startrow-1;
-		}
-		
+		return $sql;
+	}
+
+	public function getDoctorListSearchCondition($search){
 		$arrcol=array();
 		$arrcol[]="d.name";
 		$arrcol[]="d.position";
@@ -68,16 +75,23 @@ inner join tb_hospital h on d.hospital_id=h.id
 		$arrcol[]="s.name";
 		$arrcol[]="cat.name";
 		$searchsql=splitCodition($arrcol,$search);
-		$sql="select distinct d.id,d.name,d.photo,d.position
-from tb_doctor d
-inner join tb_hospital h on d.hospital_id=h.id and h.status='A'
-inner join tb_department dp on d.department_id=dp.id and dp.status='A'
-inner join tb_college c on dp.college_id=c.id and c.status='A'
-inner join rc_department_subcategory rc_ds on dp.id=rc_ds.pid
-inner join tb_subcategory s on rc_ds.fid=s.id
-inner join tb_category cat on cat.id=s.category_id
- where d.status='A' 
+
+		return $searchsql;
+	}
+
+	public function getDoctorList($search,$page,$member_id){
+		
+		$startrow=($page-1)*18;
+		if($startrow>0){
+			//$startrow=$startrow-1;
+		}
+		
+		
+		$searchsql=$this->getDoctorListSearchCondition($search);
+		$realsql=$this->getDoctorListSearchSql();
+		$sql="$realsql
  and $searchsql
+ where d.status='A'
   order by d.seq
 		limit $startrow,18";
 		$query = $this->dbmgr->query($sql);
@@ -87,31 +101,57 @@ inner join tb_category cat on cat.id=s.category_id
 	}
 
 	public function getDoctorListPageCount($search){
-		$arrcol=array();
-		$arrcol[]="d.name";
-		$arrcol[]="d.position";
-		$arrcol[]="d.content";
-		$arrcol[]="h.name";
-		$arrcol[]="dp.name";
-		$arrcol[]="c.name";
-		$arrcol[]="s.name";
-		$arrcol[]="cat.name";
-		$searchsql=splitCodition($arrcol,$search);
-		$sql="select sum(1) doctor_count from (select  distinct d.id
-from tb_doctor d
-inner join tb_hospital h on d.hospital_id=h.id and h.status='A'
-inner join tb_department dp on d.department_id=dp.id and dp.status='A'
-inner join tb_college c on dp.college_id=c.id and c.status='A'
-inner join rc_department_subcategory rc_ds on dp.id=rc_ds.pid
-inner join tb_subcategory s on rc_ds.fid=s.id
-inner join tb_category cat on cat.id=s.category_id
- where d.status='A'  
+		$searchsql=$this->getDoctorListSearchCondition($search);
+		$realsql=$this->getDoctorListSearchSql();
+		$sql="select sum(1) doctor_count from 
+		( $realsql  
+ where d.status='A'
  and $searchsql ) a";
 		$query = $this->dbmgr->query($sql);
 		$result = $this->dbmgr->fetch_array($query); 
 		return $result["doctor_count"];
 	}
+	
 
+	public function getFollowDoctorList($member_id,$search,$page,$member_id){
+		
+		$startrow=($page-1)*18;
+		if($startrow>0){
+			//$startrow=$startrow-1;
+		}
+		
+		
+		$searchsql=$this->getDoctorListSearchCondition($search);
+		$realsql=$this->getDoctorListSearchSql();
+		$realsql="$realsql
+inner join tb_member_follow_doctor mfd on mfd.doctor_id=d.id and mfd.member_id=$member_id";
+
+		$sql="$realsql
+ and $searchsql
+ where d.status='A'
+  order by d.seq
+		limit $startrow,18";
+		$query = $this->dbmgr->query($sql);
+		$result = $this->dbmgr->fetch_array_all($query); 
+
+		return $result;
+	}
+
+	public function getFollowDoctorListPageCount($member_id,$search){
+
+		$searchsql=$this->getDoctorListSearchCondition($search);
+		$realsql=$this->getDoctorListSearchSql();
+		$realsql="$realsql
+inner join tb_member_follow_doctor mfd on mfd.doctor_id=d.id and mfd.member_id=$member_id";
+
+		$sql="select sum(1) doctor_count from 
+		( $realsql  
+ where d.status='A'
+ and $searchsql ) a";
+		$query = $this->dbmgr->query($sql);
+		$result = $this->dbmgr->fetch_array($query); 
+		return $result["doctor_count"];
+	}
 
  }
  
