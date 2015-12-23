1508b447ea8e1d14065dc0667d96c55f51113c47
Steve Cai <steve.cai@morningstar.com>
2015-12-23 11:27:02 +0800

    Add USE NCMI151123

--- dbscript/ncmi151123_tb_user.sql
+++ dbscript/ncmi151123_tb_user.sql
@@ -4,6 +4,7 @@
 -- ------------------------------------------------------
 -- Server version	5.6.21-enterprise-commercial-advanced-log
 
+use ncmi151123;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
--- dbscript/ncmi151123_tb_website_base.sql
+++ dbscript/ncmi151123_tb_website_base.sql
@@ -3,7 +3,7 @@
 -- Host: www.myhkdoc.com    Database: ncmi151123
 -- ------------------------------------------------------
 -- Server version	5.6.21-enterprise-commercial-advanced-log
-
+use ncmi151123;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
