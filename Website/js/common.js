0468dd25c3f17a8899f912442bf08c15536d75a9
steve.cai <alucard263096@126.com>
2016-01-10 00:39:50 +0800

    嵌入代码

--- Website/js/common.js
+++ Website/js/common.js
@@ -12,3 +12,15 @@ function validatemobile(mobile) {
     }
     return true;
 }
+function checkURL(URL) {
+    var str = URL;
+    //判断URL地址的正则表达式为:http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?
+    //下面的代码中应用了转义字符"\"输出一个字符"/"
+    var Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+    var objExp = new RegExp(Expression);
+    if (objExp.test(str) == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
